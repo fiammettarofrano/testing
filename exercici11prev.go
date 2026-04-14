b64d361d84e78ac05a072391b50082b1fb8ff1ed
fiammettarofrano <fiammetta.rofrano@gmail.com>
2026-04-14 10:56:56 +0200

    file test to delete

---
+++ exercici11prev.go
@@ -0,0 +1,10 @@
+//Heu de realitzar una estructura condicional Switch que contempli els seguents case basant-se en un ascensor d'un centre comercial:
+// 1. Planta primera i tercera, mostrara el missatge "Articles Llar".
+// 2. Planta segona, mostrara el missatge "Moda infantil".
+// 3. Planta cuarta, mostrara el missatge "Joguines"
+// 4. Un default, mostrant un error.
+package main
+
+func main() {
+
+}
